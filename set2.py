436bb11b0fd4ae71a54f8a7324185dd0a6c2ebdb
Spencer Manning <spencerhmanning@gmail.com>
2024-12-01 20:09:38 -0500

    Typo

--- set2.py
+++ set2.py
@@ -3,28 +3,28 @@ import base64
 def s2_ch1():
     print(f"s2_ch1")
 
-    
 
-def s2_ch1():
-    print(f"s2_ch1")
 
-def s2_ch1():
-    print(f"s2_ch1")
+def s2_ch2():
+    print(f"s2_ch2")
 
-def s2_ch1():
-    print(f"s2_ch1")
+def s2_c3():
+    print(f"s2_ch3")
 
-def s2_ch1():
-    print(f"s2_ch1")
+def s2_ch4():
+    print(f"s2_ch4")
 
-def s2_ch1():
-    print(f"s2_ch1")
+def s2_ch5():
+    print(f"s2_ch5")
 
-def s2_ch1():
-    print(f"s2_ch1")
+def s2_ch6():
+    print(f"s2_ch6")
 
-def s2_ch1():
-    print(f"s2_ch1")
+def s2_ch7():
+    print(f"s2_ch7")
+
+def s2_ch8():
+    print(f"s2_ch8")
 
 if __name__ == "__main__":
     print("\n------------------ Start: ------------------")
