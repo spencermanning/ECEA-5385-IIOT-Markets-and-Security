700613ed1704e90d4bddb28696ecf5d7a44f08e9
Spencer Manning <spencerhmanning@gmail.com>
2024-12-01 20:07:51 -0500

    Lay out set2

---
+++ set2.py
@@ -0,0 +1,41 @@
+import base64
+
+def s2_ch1():
+    print(f"s2_ch1")
+
+    
+
+def s2_ch1():
+    print(f"s2_ch1")
+
+def s2_ch1():
+    print(f"s2_ch1")
+
+def s2_ch1():
+    print(f"s2_ch1")
+
+def s2_ch1():
+    print(f"s2_ch1")
+
+def s2_ch1():
+    print(f"s2_ch1")
+
+def s2_ch1():
+    print(f"s2_ch1")
+
+def s2_ch1():
+    print(f"s2_ch1")
+
+if __name__ == "__main__":
+    print("\n------------------ Start: ------------------")
+
+    s2_ch1()
+    # s2_ch2()
+    # s2_ch3()
+    # s2_ch4()
+    # s2_ch5()
+    # s2_ch6()
+    # s2_ch7()
+    # s2_ch8()
+
+    print("\n------------------ End: ------------------")
