9c4fa46d1322c96b2b047f02c12b714a9b3ea518
Spencer Manning <spencerhmanning@gmail.com>
2024-12-01 20:32:33 -0500

    Finish ch9

--- set2.py
+++ set2.py
@@ -1,41 +1,43 @@
 import base64
+from Crypto.Util.Padding import pad, unpad  # for ch1
+def s2_ch9():
+    print(f"s2_ch9")
+    msg = b"YELLOW SUBMARINE"
+    padded_msg = pad(msg, 20)
+    print(padded_msg)
 
-def s2_ch1():
-    print(f"s2_ch1")
 
-
-
-def s2_ch2():
-    print(f"s2_ch2")
+def s2_ch10():
+    print(f"s2_ch10")
 
 def s2_c3():
-    print(f"s2_ch3")
+    print(f"s2_ch11")
 
-def s2_ch4():
-    print(f"s2_ch4")
+def s2_ch12():
+    print(f"s2_ch12")
 
-def s2_ch5():
-    print(f"s2_ch5")
+def s2_ch13():
+    print(f"s2_ch13")
 
-def s2_ch6():
-    print(f"s2_ch6")
+def s2_ch14():
+    print(f"s2_ch14")
 
-def s2_ch7():
-    print(f"s2_ch7")
+def s2_ch15():
+    print(f"s2_ch15")
 
-def s2_ch8():
-    print(f"s2_ch8")
+def s2_ch16():
+    print(f"s2_ch16")
 
 if __name__ == "__main__":
     print("\n------------------ Start: ------------------")
 
-    s2_ch1()
-    # s2_ch2()
-    # s2_ch3()
-    # s2_ch4()
-    # s2_ch5()
-    # s2_ch6()
-    # s2_ch7()
-    # s2_ch8()
+    s2_ch9()
+    # s2_ch10()
+    # s2_ch11()
+    # s2_ch12()
+    # s2_ch13()
+    # s2_ch14()
+    # s2_ch15()
+    # s2_ch16()
 
     print("\n------------------ End: ------------------")
